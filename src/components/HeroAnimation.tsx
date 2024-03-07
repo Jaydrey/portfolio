e5512f48f61691a9a3b0420ebaaa9d4b1bed2da1
Jaydrey <jaribwetshi64@gmail.com>
2024-03-07 19:47:23 +0300

    feat(component): created animation component that display lottie animations

---
+++ src/components/HeroAnimation.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useEffect, createRef } from "react";
+import lottie from "lottie-web";
+
+const HeroAnimation = ({ lottie_path }: { lottie_path: string}) => {
+  let animationContainer = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const animate = lottie.loadAnimation({
+      container: animationContainer.current as HTMLDivElement,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      path: lottie_path,
+    });
+
+    return () => animate.destroy();
+  }, []);
+  return <div className="w-full h-full" ref={animationContainer}></div>;
+};
+
+export default HeroAnimation;
