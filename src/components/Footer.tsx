8301deaa0212146c6931307b098b0592f65b2df0
Jaydrey <jaribwetshi64@gmail.com>
2024-03-07 19:45:46 +0300

    feat(component): added the footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <footer className="w-full flex justify-center bg-transparent py-2">
+      <div className="w-[90%] md:w-[80%] lg:w-[75%] flex flex-col items-center">
+        <div>
+          <p className="text-lg text-slate-200">Made with ❤️ by Jarib Wetshi </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
